be85da5263da1a97b43ebf53a20bd4c4ccfdb6c5
kodaishiotsuki <0711dr5@gmail.com>
2022-10-27 15:43:05 +0900

    汎用関数(property.ts)

--- utils/property.ts
+++ utils/property.ts
@@ -1,4 +1,4 @@
-import { RichTextType } from "../types/types";
+import { PageType, RichTextType } from "../types/types";
 
 //汎用関数
 
@@ -11,4 +11,29 @@ export const getText = (richTextArr: RichTextType[]) => {
     console.log(err);
   }
   return ""; //最終的に文字列を返す
+};
+
+//coverのurlを取得
+export const getCover = (cover: PageType["cover"]) => {
+  if (cover && cover.file) return cover.file.url;
+  if (cover && cover.external) return cover.external.url;
+  return "/noimage.png";
+};
+
+//publishedの日付を取得
+export const getDate = (date: { start: string }) => {
+  try {
+    return date.start;
+  } catch (err) {
+    console.log(err);
+  }
+  return "-";
+};
+
+//tagsのmulti_selectを取得
+export const getMultiSelect = (multiSelect: [{ name: string }]) => {
+  try {
+    return multiSelect.map((tag) => tag.name);
+  } catch (err) {}
+  return [];
 };
